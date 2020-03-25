ea2e06a3c5afd52edaacef26136d8900e7f4d0de
Siyun Wang <siyun@Siyuns-MacBook-Pro.local>
2020-03-25 14:19:10 -0700

    Update css for main app

--- server/index.js
+++ server/index.js
@@ -2,28 +2,31 @@ const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const PORT = 3333;
+
 const app = express();
-app.use(express.static(path.join(__dirname, '/../public')));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 
-const galleryProxy = createProxyMiddleware({target: 'http://localhost:3002', changeOrigin: true});
-app.use('/seeAllPhotos/**', galleryProxy);
+const galleryProxy = createProxyMiddleware({target: 'http://localhost:3004', changeOrigin: true});
+app.use('/seeAllPhotos', galleryProxy);
+// app.use('/seeAllPhotos/:photoId', galleryProxy);
+
 
 const restaurantProxy = createProxyMiddleware({target: 'http://localhost:8000', changeOrigin: true});
 app.use('/currentRestaurant', restaurantProxy);
 app.use('/restaurant', restaurantProxy);
 
 const dishesProxy = createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true});
-app.use('/getCompany', dishesProxy);
-app.use('/getItems', dishesProxy);
-app.use('/getPhotos', dishesProxy);
-app.use('/getReviews', dishesProxy);
+app.use('/popularDishes/**', dishesProxy);
 
 const reviewsProxy = createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true});
 app.use('/api/restaurants/**', reviewsProxy);
 app.use('/api/restaurant/**', reviewsProxy);
 
-const PORT = 3333;
+
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 });
